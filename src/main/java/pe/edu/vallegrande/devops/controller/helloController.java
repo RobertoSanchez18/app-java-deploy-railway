d4c15f17b5524bae7fdbbdd85fbaf359367e3a0f
RobertoSanchez18 <roberto.sanchez@vallegrande.edu.pe>
2025-03-27 15:03:28 -0500

    add pipeline

---
+++ src/main/java/pe/edu/vallegrande/devops/controller/helloController.java
@@ -0,0 +1,16 @@
+package pe.edu.vallegrande.devops.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+public class helloController {
+
+    @GetMapping
+    public String saludo() {
+        return "Hello World";
+    }
+
+}
---
+++ src/test/java/pe/edu/vallegrande/devops/DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
